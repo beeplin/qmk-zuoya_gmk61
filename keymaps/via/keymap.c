f3a428fd1d4e600ea1652029c073395d6b410721
beeplin <beeplin@gmail.com>
2025-03-20 17:48:49 +0800

    Fix keymap layout by updating key positions for improved functionality in ZUOYA GMK61 configuration

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -17,8 +17,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_LGUI,  KC_LALT,                      KC_NO,                                  KC_LALT,  KC_INS,   KC_RCTL,            TO(2)
     ),
     [2] = LAYOUT_tkl_ansi(
-        KC_TILD,        KC_LT,    KC_MINS,  KC_EQL,   KC_GT,    KC_GRV,   KC_PERC,  KC_AMPR,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_PIPE,  KC_EXLM,
-        KC_UNDS,        KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+        KC_TILD,        KC_LT,    KC_MINS,  KC_EQL,   KC_GT,    KC_PIPE,  KC_PERC,  KC_AMPR,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_HASH,  KC_EXLM,
+        KC_GRV ,        KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
  LCTL_T(KC_CAPS),       KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_PLUS,
         KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
         KC_LCTL,        KC_LGUI,  KC_LALT,                LT(3, KC_SPC),                                KC_LALT,  KC_RGUI,  KC_RCTL,            MO(4)
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_INS,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_VOLD,  KC_VOLU,  KC_MUTE,
  LALT_T(KC_F11), LCTL_T(KC_HOME), KC_BSPC,  KC_UP,    KC_PGUP,  KC_DEL,   KC_CIRC,  KC_7,     KC_8,     KC_9,     KC_DLR,   KC_MPRV,  KC_MNXT,  KC_MPLY,
  LCTL_T(KC_F12), LSFT_T(KC_ESC),  KC_LEFT,  KC_ENT,   KC_RGHT,  KC_PGDN,  KC_AT,    KC_4,     KC_5,     KC_6,     KC_0,     KC_APP,             KC_SCRL,
-        KC_LSFT, LALT_T(KC_END),  KC_TAB,   KC_DOWN,  KC_WH_L,  KC_WH_R,  KC_HASH,  KC_1,     KC_2,     KC_3,     KC_ASTR,                      KC_RSFT,
+        KC_LSFT, LALT_T(KC_END),  KC_TAB,   KC_DOWN,  KC_WH_L,  KC_WH_R,  KC_UNDS,  KC_1,     KC_2,     KC_3,     KC_ASTR,                      KC_RSFT,
  LCTL_T(KC_BRID),       KC_LGUI,  KC_LALT,                      KC_NO,                                  KC_LALT,  KC_RGUI,  RCTL_T(KC_BRIU),    TO(0)
     ),
     [4] = LAYOUT_tkl_ansi(
