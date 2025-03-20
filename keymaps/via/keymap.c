b48b9d42fee4702903046b374eb65050af928e75
beeplin <beeplin@gmail.com>
2025-03-20 18:35:42 +0800

    Enable hold on other key press and caps word functionality in ZUOYA GMK61 configuration

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD,        KC_LT,    KC_MINS,  KC_EQL,   KC_GT,    KC_PIPE,  KC_PERC,  KC_AMPR,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_HASH,  KC_EXLM,
         KC_GRV ,        KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
  LCTL_T(KC_CAPS),       KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_PLUS,
-        KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
+        KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      CW_TOGG,
         KC_LCTL,        KC_LGUI,  KC_LALT,                LT(3, KC_SPC),                                KC_LALT,  KC_RGUI,  KC_RCTL,            MO(4)
     ),    
     [3] = LAYOUT_tkl_ansi(
