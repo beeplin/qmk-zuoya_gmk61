68550ee00f27bdf88ef8cd40bbdc3ea16023ad30
beeplin <beeplin@gmail.com>
2025-10-09 15:55:35 +0800

    new

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -11,30 +11,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_tkl_ansi(
         KC_GRV,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_VOLD,  KC_VOLU,  KC_MUTE,
-        KC_F11,         KC_HOME,  KC_BSPC,  KC_UP,    KC_DEL,   KC_PGUP,  KC_SLSH,  KC_7,     KC_8,     KC_9,     KC_MINS,  KC_MPRV,  KC_MNXT,  KC_MPLY,
-        KC_F12,         KC_END,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_ASTR,  KC_4,     KC_5,     KC_6,     KC_PLUS,  KC_BRID,            KC_BRIU,
-        KC_LSFT,        C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  KC_ENT,   KC_0,     KC_1,     KC_2,     KC_3,     KC_DOT,                       KC_RSFT,
+        KC_F11,         KC_PGUP,  KC_BSPC,  KC_UP,    KC_DEL,   KC_HOME,  KC_SLSH,  KC_7,     KC_8,     KC_9,     KC_PLUS,  KC_MPRV,  KC_MNXT,  KC_MPLY,
+        KC_F12,         KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_END,   KC_ASTR,  KC_4,     KC_5,     KC_6,     KC_0,     KC_BRID,            KC_BRIU,
+        KC_LSFT,        C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  KC_ENT,   KC_MINS,  KC_1,     KC_2,     KC_3,     KC_DOT,                       KC_RSFT,
         KC_LCTL,        KC_LGUI,  KC_LALT,                      KC_NO,                                  KC_LALT,  KC_INS,   KC_RCTL,            TO(2)
     ),
     [2] = LAYOUT_tkl_ansi(
-        KC_TILD,        KC_LT,    KC_MINS,  KC_EQL,   KC_GT,    KC_PIPE,  KC_PERC,  KC_AMPR,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_HASH,  KC_EXLM,
-        KC_GRV ,        KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
- LCTL_T(KC_CAPS),       KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_PLUS,
-        KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      CW_TOGG,
-        KC_LCTL,        KC_LGUI,  KC_LALT,                LT(3, KC_SPC),                                KC_LALT,  KC_RGUI,  KC_RCTL,            MO(4)
+        KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,         KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+ LCTL_T(KC_ESC),        KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,
+        KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
+ LCTL_T(KC_CAPS),       KC_LGUI,  KC_LALT,                LT(3, KC_SPC),                                KC_LALT,  KC_APP,   KC_RCTL,            MO(4)
     ),    
     [3] = LAYOUT_tkl_ansi(
-        KC_INS,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_VOLD,  KC_VOLU,  KC_MUTE,
- LALT_T(KC_F11), LCTL_T(KC_HOME), KC_BSPC,  KC_UP,    KC_PGUP,  KC_PGDN,  KC_CIRC,  KC_7,     KC_8,     KC_9,     KC_DLR,   KC_MPRV,  KC_MNXT,  KC_MPLY,
- LCTL_T(KC_F12), LALT_T(KC_ESC),  KC_LEFT,  KC_ENT,   KC_RGHT,  KC_TAB,   KC_AT,    KC_4,     KC_5,     KC_6,     KC_0,     KC_APP,             KC_SCRL,
-        KC_LSFT, LSFT_T(KC_END),  KC_DEL,   KC_DOWN,  KC_WH_L,  KC_WH_R,  KC_UNDS,  KC_1,     KC_2,     KC_3,     KC_ASTR,                      KC_RSFT,
- LCTL_T(KC_BRID),       KC_LGUI,  KC_LALT,                      KC_NO,                                  KC_LALT,  KC_RGUI,  RCTL_T(KC_BRIU),    TO(0)
+        KC_F11,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_VOLD,  KC_VOLU,  KC_MUTE,
+        KC_F12,         KC_PGUP,  KC_BSPC,  KC_UP,    KC_DEL,   KC_GRV,   KC_BSLS,  KC_7,     KC_8,     KC_9,     KC_EQL,   KC_MPRV,  KC_MNXT,  KC_MPLY,
+        KC_LCTL,        KC_LALT,  KC_LEFT,  KC_ENT,   KC_RGHT,  KC_TAB,   KC_QUOT,  KC_4,     KC_5,     KC_6,     KC_0,     KC_HOME,            KC_END,
+        KC_LSFT,        KC_PGDN,  KC_WH_L,  KC_DOWN,  KC_WH_R,  KC_LBRC,  KC_RBRC,  KC_1,     KC_2,     KC_3,     KC_MINS,                      KC_RSFT,
+        CW_TOGG,        KC_LGUI,  KC_LALT,                      KC_NO,                                  KC_LALT,  KC_INS,   KC_RCTL,            TO(0)
     ),
     [4] = LAYOUT_tkl_ansi(
-        KC_GRV,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   U_EE_CLR,
-        KC_TAB,         MD_BLE1,  MD_BLE2,  MD_BLE3,  MD_24G,   KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_I,     KC_O,     KC_P,     RGB_SPD,  RGB_SPI,  RGB_MOD,
-        KC_CAPS,        TO(0),    TO(2),    KC_D,     KC_F,     KC_INS,   KC_HOME,  KC_PGUP,  KC_K,     KC_L,     RGB_HUD,  RGB_HUI,            KC_ENT,
-        KC_LSFT,        LOGO_MOD, LOGO_HUI, RGB_SAD,  RGB_SAI,  KC_DEL,   KC_END,   KC_PGDN,  RGB_VAD,  RGB_VAI,  KC_UP,                        QK_BAT,
-        KC_LCTL,        QK_WLO,   KC_LALT,                      RGB_RTOG,                               KC_LEFT,  KC_DOWN,  KC_RGHT,            KC_NO
+        LOGO_TOG,       LOGO_RMOD,LOGO_MOD, LOGO_HUD, LOGO_HUI, LOGO_SAD, LOGO_SAI, LOGO_VAD, LOGO_VAI, LOGO_SPD, LOGO_SPI, KC_BRID,  KC_BRIU,  KC_PSCR,
+        MD_USB,         MD_BLE1,  MD_BLE2,  MD_BLE3,  MD_24G,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_SCRL,
+        KC_NO,          TO(0),    TO(2),    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_PAUS,
+        RGB_RMOD,       RGB_MOD,  RGB_HUD,  RGB_HUI,  RGB_SAD,  RGB_SAI,  RGB_VAD,  RGB_VAI,  RGB_SPD,  RGB_SPI,  KC_UP,                        KC_NO,
+        U_EE_CLR,       QK_WLO,   QK_BAT,                       RGB_RTOG,                               KC_LEFT,  KC_DOWN,  KC_RGHT,            KC_NO
     )
 };
