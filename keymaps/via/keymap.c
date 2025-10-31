9dec0a3ade4a299bc92c70a83eacc6dcfd3154c0
beeplin <beeplin@gmail.com>
2025-11-01 00:15:01 +0800

    Update keymap.c

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -12,16 +12,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_tkl_ansi(
         KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
         KC_TAB,         KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
- LCTL_T(KC_CAPS),       KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,
-        KC_LSFT,        KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
-        KC_LCTL,        KC_LGUI,  KC_LALT,                LT(2, KC_SPC),                                KC_LALT,  KC_APP,   KC_RCTL,            MO(4)
+ LCTL_T(KC_ESC),        KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,
+        KC_LSFT,        KC_X,     KC_C,     KC_D,     KC_Z,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
+        KC_CAPS,        KC_LGUI,  KC_LALT,                LT(2, KC_SPC),                                KC_LALT,  KC_APP,   KC_RCTL,            MO(4)
     ),
     [2] = LAYOUT_tkl_ansi(
         KC_F11,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_VOLD,  KC_VOLU,  KC_MUTE,
- LALT_T(KC_F12),        KC_ESC,   KC_BSPC,  KC_UP,    KC_DEL,   KC_PGUP,  KC_HOME,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MPRV,  KC_MNXT,  KC_MPLY,
-        KC_LCTL,        KC_ENT,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,   KC_4,     KC_5,     KC_6,     KC_MINS,  KC_EQL,             CW_TOGG,
-        KC_LSFT,        KC_BTN2,  KC_BTN3,  KC_BTN1,  KC_WH_L,  KC_WH_R,  KC_UNDS,  KC_1,     KC_2,     KC_3,     KC_GRV,                       KC_RSFT,
- LCTL_T(KC_BTN4),LGUI_T(KC_BTN5), KC_LALT,                      KC_NO,                                  KC_LALT,  KC_INS,   KC_RCTL,            KC_NO
+        KC_F12,         KC_ESC,   KC_BSPC,  KC_UP,    KC_DEL,   KC_PGUP,  KC_HOME,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MPRV,  KC_MNXT,  KC_MPLY,
+ LCTL_T(KC_WH_U),       KC_ENT,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,   KC_4,     KC_5,     KC_6,     KC_MINS,  KC_EQL,             CW_TOGG,
+ LSFT_T(KC_WH_D),       KC_BTN2,  KC_BTN3,  KC_BTN1,  KC_SPC,   KC_APP,   KC_UNDS,  KC_1,     KC_2,     KC_3,     KC_GRV,                       KC_RSFT,
+ LCTL_T(KC_BTN4),LGUI_T(KC_BTN5), KC_LALT,                      KC_NO,                                  KC_LALT,  KC_APP,   KC_RCTL,            KC_NO
     ),
     [3] = LAYOUT_tkl_ansi(
         KC_F11,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_VOLD,  KC_VOLU,  KC_MUTE,
