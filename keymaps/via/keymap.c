357454d8d8da6eed231dc92c21922e540d2e51f7
beeplin <beeplin@gmail.com>
2025-11-16 10:41:11 +0800

    jk

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -11,9 +11,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_tkl_ansi(
         KC_F11,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_BTN4,  KC_BTN5,  KC_WH_U,
-        KC_TAB,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_WH_L,  KC_WH_R,
+        KC_TAB,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_K,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_WH_L,  KC_WH_R,
         KC_LCTL,   KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_WH_D,
-        KC_LSFT,   KC_X,     KC_C,     KC_D,     KC_Z,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
+        KC_LSFT,   KC_X,     KC_C,     KC_D,     KC_Z,     KC_V,     KC_J,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
   LT(3, KC_CAPS),  KC_LGUI,  KC_LALT,                LT(2, KC_SPC),                                KC_RALT,  KC_APP,   KC_RCTL,      LT(4, KC_GRV)
     ),
     [2] = LAYOUT_tkl_ansi(
